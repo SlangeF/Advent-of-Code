22ca107b83dd86906a53eb3072e68bd9c6121454
Femke Slangen <slangef@gmail.com>
2023-03-06 13:25:51 +0100

    finished year 2022 - day 10

--- year_2022/day_10/elf.py
+++ year_2022/day_10/elf.py
@@ -1,8 +1,36 @@
+def complete_cycle(cycle, x, signal_strength, screen):
+    if cycle in range(1,241,40): screen.append([])
+    screen[(cycle-1)//40].append((cycle-1)%40 in range(x-1,x+2))
+    if cycle in range(20,260,40): signal_strength[cycle] = cycle * x
+    cycle += 1
+    return cycle
+
+def print_line(line):
+    for pixel in line:
+        if pixel: print("#", end='')
+        else: print(".", end='')
+
+def print_screen(screen):
+    for line in screen:
+        print_line(line)
+        print()
+
 file = open("input.txt", "r")
 lines = file.read()
-
+x = 1
+cycle = 1
+signal_strength = {}
+screen = []
 for line in lines.split("\n")[0:-1]:
-    print(line)
+    if (line == "noop"):
+        cycle = complete_cycle(cycle, x, signal_strength, screen)
+    else:
+        for i in (0,1): cycle = complete_cycle(cycle, x, signal_strength, screen)
+        x+= int(line.split(" ")[1])
 
-print ("result first  puzzle:", "not started")
-print ("result second puzzle:", "not started")
+result = 0
+for key in signal_strength: result += signal_strength[key]
+
+print ("result first  puzzle:", result)
+print ("result second puzzle:")
+print_screen(screen)
